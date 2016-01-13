75b46fa9f114bada2c4aed72e85ba15921130c90
wizages <wizagesmax@gmail.com>
2016-01-13 14:08:40 +0000

    Added makefile examples to my folder.

---
+++ Examples/functions.cpp
@@ -0,0 +1,12 @@
+#include "functions.h"
+
+void fn1(int a)
+{
+   cout << "In function 1(" << a << ")\n";
+   return;
+}
+
+void fn2(int b)
+{
+   cout << "In function 2(" << b << ")\n";
+}
---
+++ Examples/functions.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+
+void fn1(int a);
+void fn2(int b);
